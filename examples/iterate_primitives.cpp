6e6d236349f1f3f686074fd57ce07f36bbc1bfae
lbinria <>
2023-11-02 12:50:34 +0100

    Add example to iterate primitives without connectivity

---
+++ examples/iterate_primitives.cpp
@@ -0,0 +1,39 @@
+/**
+ * This example shows how to iterate primitives over a mesh without connectivity
+*/
+#include "helpers.h"
+#include <ultimaille/all.h>
+
+
+using namespace UM;
+
+int main(int argc, char** argv) {
+
+    // Get path of current executable
+    std::string path = getExecutablePath();
+
+    // Declare a mesh with triangle surface
+    Triangles m;
+    // Loading catorus.geogram into m
+    read_by_extension(path + "/assets/catorus.geogram", m);
+
+    // Iterate over vertices
+    std::cout << "iter over vertices: " << std::endl;
+    for (auto v : m.iter_vertices()) {
+        std::cout << "( " << v.pos() << ")" << std::endl;
+    }
+
+    // Iterate over facets
+    std::cout << "iter over facets: " << std::endl;
+    for (auto f : m.iter_facets()) {
+        std::cout << f << std::endl;
+    }
+
+    // Iterate over hald-edges
+    std::cout << "iter over half-edges: " << std::endl;
+    for (auto he : m.iter_halfedges()) {
+        std::cout << he << std::endl;
+    }
+
+    return 0;
+}

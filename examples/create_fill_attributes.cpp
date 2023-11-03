2e35f6961d19fdf07feebe54c8b997f1dfe39994
lbinria <>
2023-11-03 14:49:08 +0100

    Add point attr example

--- examples/create_fill_attributes.cpp
+++ examples/create_fill_attributes.cpp
@@ -20,19 +20,35 @@ int main(int argc, char** argv) {
     // Loading catorus.geogram into m
     read_by_extension(path + "/assets/catorus.geogram", m);
 
-    // --- FACE ATTR ---
+    // --- POINT ATTR ---
+
+    // Create a point attribute
+    PointAttribute<double> pa(m.points);
+
+    for (auto v : m.iter_vertices()) {
+        // Compute manhattan distance (l1-norm) between origin and vertex
+        auto d = vec3(0,0,0) - v.pos();
+        pa[v] = std::abs(d.x) + std::abs(d.y) + std::abs(d.z);
+    }
+
+    // --- SAVE POINT ---
+
+    // Save mesh with all previously created attributes
+    write_by_extension("catorus_manhattan.geogram", m, {{{"pa", pa.ptr}}, {}, {}});
+
+    // --- FACET ATTR ---
 
     // Create a facet attribute
     FacetAttribute<int> fa(m);
 
-    // For all facets in mesh, associate a random int value between 0-100
+    // For all facets in mesh, associate a random int value between [0-99]
     for (auto f : m.iter_facets())
         fa[f] = rand() % 100;
 
-    // --- SAVE ---
+    // --- SAVE FACET ---
 
     // Save mesh with all previously created attributes
-    write_by_extension("catorus_attr.geogram", m, {{}, {{"fa", fa.ptr}}, {}});
+    write_by_extension("catorus_fa.geogram", m, {{}, {{"fa", fa.ptr}}, {}});
 
     // --- END ---
 

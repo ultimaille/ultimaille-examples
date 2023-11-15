44d03d6f13d073631e11df80b8d720658f12f19e
lbinria <>
2023-11-15 14:18:51 +0100

    Cross platform

--- examples/helpers.h
+++ examples/helpers.h
@@ -1,4 +1,7 @@
 #include <iostream>
+
+#ifdef __unix__
+
 #include <unistd.h>
 
 std::string getExecutablePath() {
@@ -17,3 +20,21 @@ std::string getExecutablePath() {
     int i = path.find_last_of("/");
     return path.substr(0, i);
 }
+
+#elif _WIN32
+
+#include <Windows.h>
+
+std::string getExecutablePath() {
+    char pBuf[256];
+    size_t len = sizeof(pBuf); 
+    int bytes = GetModuleFileName(NULL, pBuf, len);
+
+    std::string path(pBuf);
+
+    // Split path, get directory name without program name
+    int i = path.find_last_of("/");
+    return path.substr(0, i);
+}
+
+#endif

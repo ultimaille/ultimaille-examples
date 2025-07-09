5a0a0344471e9bc6648aafa25fd4762802585e5a
lbinria <77280433+lbinria@users.noreply.github.com>
2025-07-09 11:11:50 +0200

    feat: add solver example

---
+++ examples/solver.cpp
@@ -0,0 +1,62 @@
+/**
+ * This example shows how to use the solver
+*/
+#include <ultimaille/all.h>
+
+
+using namespace UM;
+
+int main() {
+	
+	const std::string input_dir = ASSETS_INPUT_DIR;
+	const std::string output_dir = OUTPUT_DIR;
+	// Load mesh from file
+	
+	Triangles m;
+	read_by_extension(input_dir + "catorus.geogram", m);
+	m.connect();
+
+	LeastSquares solver(m.nverts() * 3);
+	
+	// Select some vertices to fix
+	// I choose ears, muzzle and legs
+	int fix_vert[] = {1249, 2378, 485, 4095, 4034};
+	for (auto vi : fix_vert) {
+		auto v = m.vertex(vi);
+		solver.fix(v * 3 + 0, v.pos().x);
+		solver.fix(v * 3 + 1, v.pos().y);
+		solver.fix(v * 3 + 2, v.pos().z);
+	}
+
+	for (auto &v : m.iter_vertices()) {
+
+		for (int dim = 0; dim < 3; dim++) {
+			LinExpr lexpr;
+
+			int n = 0;
+			for (auto &h : v.iter_halfedges()) {
+				auto neighbor = h.to();
+				lexpr += Linear::X(neighbor * 3 + dim);
+				n++;
+			}
+
+			lexpr *= (1.0 / n);
+			solver.add_to_energy(Linear::X(v * 3 + dim) - lexpr);
+		}
+	}
+
+	// Solve
+	solver.solve();
+
+	// Set values
+	for (auto &v : m.iter_vertices()) {
+		double x = solver.value(v * 3);
+		double y = solver.value(v * 3 + 1);
+		double z = solver.value(v * 3 + 2);
+		v.pos() = {x, y, z};
+	}
+
+	write_by_extension(output_dir + "catorus_solved.geogram", m);
+
+	return 0;
+}
